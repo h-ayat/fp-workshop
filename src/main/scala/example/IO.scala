f1f877a1c6627e510af561338b8160fc80d1215b
S.H.Ayat <fahim.ayat@gmail.com>
2020-10-28 12:17:17 +0330

    IO1

--- src/main/scala/example/IO.scala
+++ src/main/scala/example/IO.scala
@@ -21,10 +21,44 @@ object IO {
     }
   }
 
-  def map[F, T, U](io: IO[F, T])(f: T => U): IO[F, U] = {
-    callback: Callback[F, U] =>
-      io(e => callback(e.map(f)))
+  def transform[F, T, F2, T2](io: IO[F, T])(
+      f: Either[F, T] => Either[F2, T2]
+  ): IO[F2, T2] = { callback: Callback[F2, T2] => io(e => callback(f(e))) }
+
+  def some[F, T, F2 >: F](io: IO[F, Option[T]])(fault: => F2): IO[F2, T] =
+    transform(io) {
+      case Right(Some(t)) => Right(t)
+      case Right(None)    => Left(fault)
+      case Left(f)        => Left(f)
+    }
+
+  def toOptional[T](io: IO[_, T]): IO[Nothing, Option[T]] =
+    transform(io) {
+      case Right(t) => Right(Some(t))
+      case _        => Right(None)
+    }
+
+  def toEither[F, T](io: IO[F, T]): IO[Nothing, Either[F, T]] =
+    transform(io)(Right.apply)
+
+  def fold[F, T, F2, T2](io: IO[F, T])(f1: F => F2, f2: T => T2): IO[F2, T2] = {
+    transform(io) {
+      case Right(t) => Right(f2(t))
+      case Left(f)  => Left(f1(f))
+    }
   }
+
+  def mapFault[F, T, F2](io: IO[F, T])(fun: F => F2): IO[F2, T] =
+    transform(io) {
+      case Right(t) => Right(t)
+      case Left(f)  => Left(fun(f))
+    }
+
+  def map[F, T, U](io: IO[F, T])(f: T => U): IO[F, U] =
+    transform(io) {
+      case Right(t) => Right(f(t))
+      case Left(f)  => Left(f)
+    }
 
   val s = simple(12)
   val s2 = map(s)(_ + 2)
@@ -33,7 +67,37 @@ object IO {
     callback: Callback[F2, U] =>
       io(_.fold(f => callback(Left(f)), t => f(t)(callback)))
   }
+}
 
+object TestLogic {
+  import scala.concurrent.ExecutionContext.Implicits.global
+
+  case class Person(id: String, name: String)
+  case class NotFoundError(id: String)
+
+  private def getUserFromDB(id: String): Future[Person] = ???
+
+  def getUser(id: String): IO[NotFoundError, Person] =
+    IO.fromFuture(getUserFromDB(id)).mapFault(_ => NotFoundError(id))
+
+  def getUser2(id: String): IO[Nothing, Option[Person]] =
+    IO.transform(IO.fromFuture(getUserFromDB(id))) {
+      case Right(t) => Right(Some(t))
+      case Left(f)  => Right(None)
+    }
+
+  def render(p: Person): String = ???
+  def prettify(html: String): String = ???
+  def optimize[F](html: String): IO[F, String] = ???
+
+  def render(id: String): IO[NotFoundError, String] =
+    for {
+      person <- IO.some(getUser2(id))(NotFoundError(id))
+      html1 = render(person)
+      optimizedHTML <- optimize(html1)
+    } yield {
+      optimizedHTML
+    }
 }
 
 object Test {
@@ -41,11 +105,12 @@ object Test {
   def writeToDB(d: Double): Future[Boolean] = ???
   import scala.concurrent.ExecutionContext.Implicits.global
 
-  val r = for {
+  val r: IO[Throwable, Boolean] = for {
     v <- IO.simple(heavy(???))
     result <- IO.fromFuture(writeToDB(v))
   } yield {
     result
   }
 
+  val a: IO[String, Int] = ???
 }
--- src/main/scala/example/package.scala
+++ src/main/scala/example/package.scala
@@ -1,12 +1,28 @@
 package object example {
 
   type Callback[-F, -T] = Either[F, T] => Unit
-  type IO[F, T] = Callback[F, T] => Unit
+  type IO[+F, +T] = Callback[F, T] => Unit
 
   implicit class IOHelper[F, T](io: IO[F, T]) {
     def map[U](f: T => U): IO[F, U] = IO.map(io)(f)
 
     def flatMap[F2 >: F, U](f: T => IO[F2, U]): IO[F2, U] =
       IO.flatMap[F, T, F2, U](io)(f)
+
+    def mapFault[F2](f: F => F2): IO[F2, T] = IO.mapFault(io)(f)
+
+    def toOption: IO[Nothing, Option[T]] = IO.toOptional(io)
+
+    def some[F2 >: F, E](
+        fault: => F2
+    )(implicit evidence: IO[F, T] <:< IO[F, Option[E]]): IO[F2, E] =
+      IO.some[F, E, F2](io)(fault)
   }
+}
+
+trait IO2[F, T] {
+  import scala.concurrent.Future
+
+  def unsafeRun(): Either[F, T]
+  def toFuture(): Future[Either[F, T]]
 }
